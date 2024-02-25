265bb29e635c8559dfb396cdef09fb61e4e62a14
Ratnesh Gupta <ratneshkrgupt@gmail.com>
2024-02-25 14:24:33 +0530

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'orange',
+    layoutTransition: {
+        name:'slide-in'
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>About page</h1>
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
